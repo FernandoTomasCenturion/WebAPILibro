6db5306114c53cbb8926b4a841af2fa1b4af298b
Tomás Centurión <t.centurion@educacionit.com>
2023-02-02 21:00:34 -0600

    agrego practica

---
+++ WebApiLibros/WebApiLibros/Data/DBLibroContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiLibros.Models;
+
+namespace WebApiLibros.Data
+{
+    public class DBLibroContext : DbContext
+    {
+        public DBLibroContext(DbContextOptions<DBLibroContext> options) : base(options) { }
+
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+
+
+    }
+}
